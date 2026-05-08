ddd4e7962d522c0a93808ff8a0f1e5c8fbfc44db
Avnit2303 <gadhiyaavnit@gmail.com>
2026-05-08 23:20:06 +0530

    ..

--- src/confing/database.js
+++ src/confing/database.js
@@ -1,6 +1,7 @@
 const monggose = require("mongoose")
 const ConnectDB = async() => {
 await monggose.connect("mongodb+srv://gadhiyaavnit:Avnit%402303@devtinder.gfw9t4x.mongodb.net/d_tindar");
+const name = "Avnit";
 }
 
 
